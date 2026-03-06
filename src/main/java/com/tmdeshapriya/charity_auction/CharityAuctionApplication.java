28a9101dcae2cad7f9bbf6a20c0d6aa2d0e002ed
tharindudeshapriya <tmdeshapriya@gmail.com>
2026-03-06 12:03:10 +0530

    Initial commit

---
+++ src/main/java/com/tmdeshapriya/charity_auction/CharityAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.tmdeshapriya.charity_auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityAuctionApplication.class, args);
+	}
+
+}
